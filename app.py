7c7e3e9d3520086d5db69c0f39f6ba404bf4e53c
gayathri-2206 <napagayathri1@gmail.com>
2026-02-21 12:24:34 +0530

    commit6

--- app.py
+++ app.py
@@ -77,6 +77,7 @@ app.config.update(
     MAIL_SERVER=os.getenv("MAIL_SERVER", "smtp.gmail.com"),
     MAIL_PORT=int(os.getenv("MAIL_PORT", 587)),
     MAIL_USE_TLS=os.getenv("MAIL_USE_TLS", "True") == "True",
+    MAIL_USE_SSL=os.getenv("MAIL_USE_SSL", "False") == "True",
     MAIL_USERNAME=os.getenv("MAIL_USERNAME"),
     MAIL_PASSWORD=os.getenv("MAIL_PASSWORD"),
     MAIL_DEFAULT_SENDER=os.getenv("MAIL_DEFAULT_SENDER")
